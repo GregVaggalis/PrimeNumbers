428bf01a91bd2a0391625deba307f79d12046eee
Greg Vaggalis <84821301+GregVaggalis@users.noreply.github.com>
2021-06-10 13:58:57 -0500

    Add files via upload

---
+++ InterviewProject/InterviewProject/Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿
+namespace InterviewProject
+{
+    partial class MainForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.MainPanel = new System.Windows.Forms.Panel();
+            this.IntContainer = new System.Windows.Forms.RichTextBox();
+            this.TogglePrimes = new System.Windows.Forms.CheckBox();
+            this.MainPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // MainPanel
+            // 
+            this.MainPanel.Controls.Add(this.IntContainer);
+            this.MainPanel.Location = new System.Drawing.Point(12, 66);
+            this.MainPanel.Name = "MainPanel";
+            this.MainPanel.Size = new System.Drawing.Size(628, 349);
+            this.MainPanel.TabIndex = 0;
+            // 
+            // IntContainer
+            // 
+            this.IntContainer.BackColor = System.Drawing.SystemColors.Menu;
+            this.IntContainer.Cursor = System.Windows.Forms.Cursors.Default;
+            this.IntContainer.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.IntContainer.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.IntContainer.Location = new System.Drawing.Point(0, 0);
+            this.IntContainer.Name = "IntContainer";
+            this.IntContainer.ReadOnly = true;
+            this.IntContainer.Size = new System.Drawing.Size(628, 349);
+            this.IntContainer.TabIndex = 0;
+            this.IntContainer.Text = "";
+            // 
+            // TogglePrimes
+            // 
+            this.TogglePrimes.AutoSize = true;
+            this.TogglePrimes.Location = new System.Drawing.Point(12, 421);
+            this.TogglePrimes.Name = "TogglePrimes";
+            this.TogglePrimes.Size = new System.Drawing.Size(62, 19);
+            this.TogglePrimes.TabIndex = 1;
+            this.TogglePrimes.Text = "Primes";
+            this.TogglePrimes.UseVisualStyleBackColor = true;
+            this.TogglePrimes.CheckedChanged += new System.EventHandler(this.TogglePrimes_CheckedChanged);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(652, 490);
+            this.Controls.Add(this.TogglePrimes);
+            this.Controls.Add(this.MainPanel);
+            this.Name = "MainForm";
+            this.Text = "Prime Integer Finder";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.MainPanel.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel MainPanel;
+        private System.Windows.Forms.RichTextBox IntContainer;
+        private System.Windows.Forms.CheckBox TogglePrimes;
+    }
+}
+
---
+++ InterviewProject/InterviewProject/Form1.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace InterviewProject
+{
+    public partial class MainForm : Form
+    {
+        int[] numAry = new int[100];        // Initialize array
+
+        public MainForm()
+        {
+            InitializeComponent();
+            IntContainer.Enter += (s, e) => { IntContainer.Parent.Focus(); };       // Shift focus away from text-box even if clicked on
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            FillArray(numAry);
+            WriteArray(numAry);
+        }
+
+        /// <summary>
+        /// This method will fill the array with number 1-100
+        /// </summary>
+        /// <param name="arry"> The array of numbers 1-100 </param>
+        public void FillArray(int[] arry)
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                arry[i] = i + 1;
+            }
+        }
+
+        /// <summary>
+        /// This method will write a standard list of number in a 10x10 grid
+        /// </summary>
+        /// <param name="arry"> The array of numbers 1-100 </param>
+        public void WriteArray(int[] arry)
+        {
+            string prtAry = "";
+            for (int i = 0; i < 100; i++)
+            {
+                prtAry += numAry[i].ToString() + "\t";
+                if ((i + 1) % 10 == 0)                  // Finds a number divisible by 10 and adds a new-line
+                {
+                    prtAry += "\n";
+                }
+            }
+
+            IntContainer.Text = prtAry;
+        }
+
+        private void TogglePrimes_CheckedChanged(object sender, EventArgs e)
+        {
+            if (TogglePrimes.Checked == true)
+            {
+                ColorPrimes(numAry);
+            }
+            else
+            {
+                WriteArray(numAry);
+            }
+        }
+
+        /// <summary>
+        /// This method will clear the text-box then appends new text with the right color
+        /// Red for primes and black for non-primes
+        /// </summary>
+        /// <param name="x"> The array of numbers 1-100 </param>
+        public void ColorPrimes(int[] x)
+        {
+            IntContainer.Clear();
+            for (int i = 1; i <= x.Length; i++)
+            {
+                // 0 and 1 are non-primes
+                if (i == 0 || i == 1)
+                {
+                    IntContainer.SelectionColor = Color.Black;      // Selects color
+                    IntContainer.AppendText(i.ToString() + "\t");   // Appends newly created text
+                }
+                else
+                {
+                    // Checks to see if a new line marker is needed
+                    if (i % 10 == 0)
+                    {
+                        if (ChkPrime(i))
+                        {
+                            IntContainer.SelectionColor = Color.Red;
+                            IntContainer.AppendText(i.ToString() + "\n");
+                        }
+                        else
+                        {
+                            IntContainer.SelectionColor = Color.Black;
+                            IntContainer.AppendText(i.ToString() + "\n");
+                        }
+                    }
+                    else
+                    {
+                        if (ChkPrime(i))
+                        {
+                            IntContainer.SelectionColor = Color.Red;
+                            IntContainer.AppendText(i.ToString() + "\t");
+                        }
+                        else
+                        {
+                            IntContainer.SelectionColor = Color.Black;
+                            IntContainer.AppendText(i.ToString() + "\t");
+                        }
+                    }
+                }
+
+            }
+        }
+
+        /// <summary>
+        /// Checks if number will be prime or not
+        /// </summary>
+        /// <param name="y"> Number that will be checked </param>
+        /// <returns> true or false; if number is prime or not</returns>
+        public bool ChkPrime(int y)
+        {
+            for (int i = 2; i < y; i++)
+            {
+                if (y % i == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
